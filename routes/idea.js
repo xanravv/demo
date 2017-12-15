1030e9eb105f88d220ecdf375147595c85946e3a
Warnax <info@warnax.co[5D[5D[5D>
2017-12-15 19:40:08 +0100

    updated colors

--- routes/idea.js
+++ routes/idea.js
@@ -68,7 +68,7 @@ router.post('/', (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
-                req.flash('success_msg', 'Video idea added');
+                req.flash('success_msg', 'Idea added');
                 res.redirect('/ideas');
             })
     }
@@ -86,7 +86,7 @@ router.put('/:id', (req, res) => {
 
         idea.save()
             .then(idea => {
-            req.flash('success_msg', 'Video idea updated');
+            req.flash('success_msg', 'Idea updated');
         res.redirect('/ideas');
         })
     });
@@ -96,7 +96,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     Idea.remove({_id: req.params.id})
     .then(() => {
-        req.flash('success_msg', 'Video idea removed');
+        req.flash('success_msg', 'Idea removed');
         res.redirect('/ideas');
     });
 });
